33f153d0b11e8e931b56bd55e8f7d3abcf31b445
Batel Oved <100batel100@gmail.com>
2020-10-24 18:29:56 +0300

    folders arrange

--- mtm/build/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: C:/Users/100ba/source/Technion_projects/mtm
-# Build directory: C:/Users/100ba/source/Technion_projects/mtm/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
